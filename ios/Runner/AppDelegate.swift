687d084139398253ab12a94567716ede35948a6e
scoolslamh <your-email@example.com>
2026-03-17 14:41:18 +0300

    23

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,21 +1,18 @@
-import Flutter
 import UIKit
-import GoogleMaps // 1. أضف هذا السطر لاستيراد المكتبة
+import Flutter
+import GoogleMaps
 
-@main
-@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
+@UIApplicationMain // استخدم هذا الوسم لضمان التوافق الكامل
+@objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    // 2. أضف هذا السطر ووضع مفتاح الـ API الخاص بك بين القوسين
+    
+    // تسجيل مفتاح خرائط جوجل
     GMSServices.provideAPIKey("AIzaSyCbjtLCziPC2AeMg7poifI710bRKveSAAI")
     
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
-
-  func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
-    GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
   }
 }
